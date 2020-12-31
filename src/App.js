5677cee3408c816c33e0f67b8f987dc269130133
Artem <rewwer356@gmail.com>
2020-12-31 19:04:04 +0300

    Rewrite with hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
-import Clock from "./components/Clock";
-import Button from "./components/Button";
+import 'antd/dist/antd.dark.css';
+import React, {useState} from "react";
+import {Button, DatePicker} from "antd";
 
 //https://jsonplaceholder.typicode.com/ fake rest api
 function App() {
+    const [date, setDate] = useState(null);
+    const [count, setCount] = useState(0);
+
+    /*useEffect(() => {
+        message.info(`Rerender! You clicked ${count} times`)
+    })
+*/
     return (
-        <>
-        <Clock/>
-        <Button/>
-        </>
+        <div className="App">
+            <DatePicker onChange={date => setDate(date)}/>
+            <div style={{marginTop: 16}}>
+                Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
+            </div>
+            <p>You clicked {count} times</p>
+            <Button onClick={() => setCount(count + 1)} type="primary">Click me</Button>
+        </div>
     );
+    /*return (
+        <div>
+        <Clock/>
+        <MainButton/>
+        </div>
+    );*/
 }
 
 export default App;
--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,16 +1,14 @@
 import React from "react";
+import {Button} from "antd";
 
-export default function Button() {
+export default function MainButton() {
     function handleClick(e) {
         e.preventDefault();
         console.log('The link was clicked.');
     }
 
     return (
-        <>
-            <button onClick={handleClick}>Log react event</button>
-
-        </>
+        <Button onClick={handleClick}>Log react event</Button>
     )
 }
 
