73293ad811d06dd9a70e27afe8625c5a80ba153a
Artem <rewwer356@gmail.com>
2020-12-31 23:01:32 +0300

    Start from scratch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import './App.css';
 import 'antd/dist/antd.dark.css';
-import React, {useState} from "react";
-import {Button, DatePicker} from "antd";
+import React, {useState, useEffect} from "react";
+import {Button} from "antd";
 
 //https://jsonplaceholder.typicode.com/ fake rest api
 function App() {
-    const [date, setDate] = useState(null);
     const [count, setCount] = useState(0);
 
-    /*useEffect(() => {
-        message.info(`Rerender! You clicked ${count} times`)
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        setIsLoading(true)
+        setInterval(null, 100)
+        return () => setIsLoading(false)
     })
-*/
+
     return (
         <div className="App">
-            <DatePicker onChange={date => setDate(date)}/>
-            <div style={{marginTop: 16}}>
-                Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-            </div>
             <p>You clicked {count} times</p>
             <Button onClick={() => setCount(count + 1)} type="primary">Click me</Button>
         </div>
     );
-    /*return (
-        <div>
-        <Clock/>
-        <MainButton/>
-        </div>
-    );*/
 }
 
 export default App;
