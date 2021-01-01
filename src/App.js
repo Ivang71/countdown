951538941ab4f4cb49ab60c992fb5d246f4fc731
Artem <rewwer356@gmail.com>
2021-01-01 11:39:13 +0300

    Add loading button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import './App.css';
 import 'antd/dist/antd.dark.css';
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {Button} from "antd";
+
+
+/*class App extends React.Component {
+    state = {
+        loadings: [],
+    };
+
+    enterLoading = index => {
+        this.setState(({ loadings }) => {
+            const newLoadings = [...loadings];
+            newLoadings[index] = true;
+
+            return {
+                loadings: newLoadings,
+            };
+        });
+        setTimeout(() => {
+            this.setState(({ loadings }) => {
+                const newLoadings = [...loadings];
+                newLoadings[index] = false;
+
+                return {
+                    loadings: newLoadings,
+                };
+            });
+        }, 6000);
+    };
+
+    render() {
+        const { loadings } = this.state;
+        return (
+            <>
+                <Button
+                    type="primary"
+                    loading={loadings[1]}
+                    onClick={() => this.enterLoading(1)}
+                >
+                    Click me!
+                </Button>
+            </>
+        );
+    }
+}*/
+
 
 //https://jsonplaceholder.typicode.com/ fake rest api
 function App() {
     const [count, setCount] = useState(0);
 
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const enterLoading = () => {
         setIsLoading(true)
-        setInterval(null, 100)
-        return () => setIsLoading(false)
-    })
+        setTimeout(() => setIsLoading(false), 2000)
+    }
 
     return (
         <div className="App">
             <p>You clicked {count} times</p>
-            <Button onClick={() => setCount(count + 1)} type="primary">Click me</Button>
+            <Button
+                onClick={() => { enterLoading(); setCount(count + 1)}}
+                type="primary"
+                loading={isLoading}
+            >Click me</Button>
         </div>
     );
 }
